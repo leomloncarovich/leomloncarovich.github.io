ecf2567ae473d4157edeb1ea251baeb76e4113bd
Leo Mosca Loncarovich <leol.mosca@gmail.com>
2024-08-19 08:12:40 -0300

    fix: Select text!

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,16 +6,17 @@ const Hero = () => {
     <div className="flex flex-wrap">
       <div className="w-full lg:w-1/2">
         <div className="flex flex-col items-center lg:items-start">
-          <h1 className="font-poppins pb-16 text-5xl tracking-tight lg:mt-16 lg:text-8xl">
+          <h1 className="font-poppins pb-16 text-5xl tracking-tight 
+          lg:mt-16 lg:text-8xl select-none pointer-events-none">
             Leo M Loncarovich
           </h1>
           <span className="bg-gradient-to-r from-rose-200 via-purple-300 
           to-violet-500 bg-clip-text text-4xl tracking-tight 
-          text-transparent font-poppins">
+          text-transparent font-poppins select-none pointer-events-none">
             Full Stack Developer
           </span>
           <p className="my-2 max-w-xl py-6 font-poppins-light 
-          tracking-tighter">
+          tracking-tighter select-none pointer-events-none">
             {HERO_CONTENT}
           </p>
         </div>
