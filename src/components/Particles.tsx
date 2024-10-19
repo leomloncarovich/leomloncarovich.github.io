5ed4a91239da68eb5f377bf341cce6da5610ad81
Leo Mosca Loncarovich <leol.mosca@gmail.com>
2024-10-19 20:18:40 -0300

    feat: Removendo o clique para aumentar a quantidade de particulas!

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -28,19 +28,12 @@ const particlesApp = () => {
       fpsLimit: 120,
       interactivity: {
         events: {
-          onClick: {
-            enable: true,
-            mode: "push",
-          },
           onHover: {
             enable: true,
             mode: "repulse",
           },
         },
         modes: {
-          push: {
-            quantity: 4,
-          },
           repulse: {
             distance: 200,
             duration: 0.4,
