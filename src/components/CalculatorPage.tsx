8b5a8d4da91bd8c5555897982ab8ea7c951d9307
Leo Mosca Loncarovich <leol.mosca@gmail.com>
2024-10-19 21:37:42 -0300

    refactor♻️: Multiplas contas na calculadora!

--- src/components/CalculatorPage.tsx
+++ src/components/CalculatorPage.tsx
@@ -3,10 +3,7 @@ import React, { useState } from 'react';
 const Calculator: React.FC = () => {
   const [isOn, setIsOn] = useState<boolean>(false);
   const [currentValue, setCurrentValue] = useState<string>('0');
-  const [pendingOperation, setPendingOperation] = useState<string | null>(null);
-  const [pendingValue, setPendingValue] = useState<string | null>(null);
   const [completeOperation, setCompleteOperation] = useState<string>('');
-
   const keypadNumbers: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
   const operations: string[] = ['+', '-', '×', '÷'];
 
@@ -17,95 +14,56 @@ const Calculator: React.FC = () => {
 
   const handleClick = (value: string) => {
     if (!isOn) return;
+    const lastChar = completeOperation.trim().slice(-1);
+    if (operations.includes(lastChar) && operations.includes(value)) {
+      setCompleteOperation((prevOperation) => prevOperation.slice(0, -1) + value);
+    } else {
+      setCompleteOperation((prevOperation) => prevOperation + value);
+    }
     setCurrentValue((prevValue) => {
-      if (value === '.' && prevValue.includes('.')) {
-        return prevValue;
-      }
       if (prevValue === '0' && value !== '.') {
         return value;
+      } else if (value === '.' && prevValue.includes('.')) {
+        return prevValue;
       } else {
         return prevValue + value;
       }
     });
-    setCompleteOperation((prevOperation) => prevOperation + value);
-  };
-
-  const handleOperation = (operation: string) => {
-    if (!isOn) return;
-    if (pendingOperation) {
-      handleCalculate();
-    }
-    setPendingOperation(operation);
-    setPendingValue(currentValue);
-    setCompleteOperation(currentValue + ' ' + operation);
-    setCurrentValue('0');
   };
 
   const handleClear = () => {
     setCurrentValue('0');
-    setPendingValue(null);
-    setPendingOperation(null);
     setCompleteOperation('');
   };
 
   const handleBackspace = () => {
     if (!isOn) return;
-    setCurrentValue((prevValue) =>
-      prevValue.length > 1 ? prevValue.slice(0, -1) : '0'
-    );
-    setCompleteOperation((prevOperation) =>
-      prevOperation.length > 1 ? prevOperation.slice(0, -1) : ''
-    );
+    setCurrentValue((prevValue) => {
+      return prevValue.length > 1 ? prevValue.slice(0, -1) : '0';
+    });
+    setCompleteOperation((prevOperation) => {
+      return prevOperation.length > 1 ? prevOperation.slice(0, -1) : '';
+    });
   };
 
   const handleCalculate = () => {
-    if (!isOn || !pendingOperation || !pendingValue) {
-      return;
+    if (!isOn) return;
+
+    try {
+      const expression = completeOperation.replace(/×/g, '*').replace(/÷/g, '/');
+      const result = eval(expression);
+      setCurrentValue(result.toString());
+      setCompleteOperation(`${expression} = ${result}`);
+      setCompleteOperation(result.toString());
+    } catch (error) {
+      setCurrentValue("Error");
+      setCompleteOperation("Error");
     }
-    const num1 = parseFloat(pendingValue);
-    const num2 = parseFloat(currentValue);
-
-    let result: number | null = null;
-
-    switch (pendingOperation) {
-      case '+':
-        result = num1 + num2;
-        break;
-      case '-':
-        result = num1 - num2;
-        break;
-      case '×':
-        result = num1 * num2;
-        break;
-      case '÷':
-        if (num2 !== 0) {
-          result = num1 / num2;
-        } else {
-          setCurrentValue("Error");
-          setCompleteOperation("Error");
-          setPendingOperation(null);
-          setPendingValue(null);
-          return;
-        }
-        break;
-
-      default:
-        break;
-    }
-
-    const finalResult = result !== null ? result : 0;
-
-    setCompleteOperation(
-      `${pendingValue} ${pendingOperation} ${currentValue} = ${result}`
-    );
-    setCurrentValue(finalResult.toString());
-    setPendingValue(null);
-    setPendingOperation(null);
   };
 
   return (
-    <main className='flex justify-center items-center'>
-      <div className="w-full max-w-md bg-gray-800 p-6 shadow-lg">
+    <main className='flex justify-center items-center m-2'>
+      <div className="w-full max-w-sm bg-gray-800 p-6 shadow-lg">
         <div className={`w-full min-h-14 border mb-4 text-right py-3 pr-2 text-lg font-mono text-white ${isOn ? 'bg-gray-700' : 'bg-gray-600'}}`}>{isOn ? completeOperation : ''}</div>
         <div className={`w-full h-14 border mb-4 text-right py-3 pr-2 text-2xl font-mono text-white ${isOn ? 'bg-gray-700' : 'bg-gray-600'}`}>{isOn ? currentValue : 'OFF'}</div>
         <div className="grid grid-cols-4 gap-4">
@@ -113,7 +71,7 @@ const Calculator: React.FC = () => {
           <button className='bg-red-500 text-white font-bold py-2 rounded' onClick={handleClear} disabled={!isOn}>AC</button>
           <button className='bg-yellow-500 text-white font-bold py-2 rounded' onClick={handleBackspace} disabled={!isOn}>C</button>       
           {operations.map((operation) => (
-            <button className='bg-blue-500 text-white font-bold py-2 rounded' key={operation} onClick={() => handleOperation(operation)} disabled={!isOn}>
+            <button className='bg-blue-500 text-white font-bold py-2 rounded' key={operation} onClick={() => handleClick(operation)} disabled={!isOn}>
               {operation}
             </button>
           ))}
